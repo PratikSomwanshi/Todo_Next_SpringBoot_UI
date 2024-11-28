5bb9f0e7ea5c6b7c171b67929b571c85e5a1fb38
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-11-28 13:43:11 +0530

    fix: delete button media query

--- components/local/todo_card/index.tsx
+++ components/local/todo_card/index.tsx
@@ -9,15 +9,15 @@ import { TodoCardProps } from "../../../lib/interfaces";
 function TodoCard(todoCardProps: TodoCardProps) {
     return (
         <div className="w-full">
-            <Card className="h-auto 300:h-20 400:h-16 py-3 w-full">
-                <div className="300:flex justify-between px-2 300:px-10 h-full space-y-4 300:space-y-0 w-full ">
+            <Card className="h-auto 700:h-20  py-2 w-full">
+                <div className="700:flex justify-between px-2 300:px-4 h-full space-y-4 300:space-y-0 w-full">
                     <div className="h-full 300:flex items-center w-full">
                         <h2 className="text:lg 300:text-xl font-medium w-full">
                             {todoCardProps.title}
                         </h2>
                     </div>
-                    <div className="400:flex  justify-between items-center h-full w-auto 400:w-40 py-2 space-y-2 300:space-y-0">
-                        <div className="flex justify-center 300:justify-end 400:h-full h-1/2 space-x-1 ">
+                    <div className="300:flex  items-center h-full w-full  py-2 space-y-2 300:space-y-0 space-x-0 300:space-x-2  ">
+                        <div className="flex justify-center items-center  400:h-full h-1/2 space-x-1 w-full 300:w-1/2">
                             <Checkbox id="completed" />
                             {/* TODO: onclick completed*/}
                             <label
@@ -27,7 +27,7 @@ function TodoCard(todoCardProps: TodoCardProps) {
                             </label>
                         </div>
                         {/* <div className="h-[2px] bg-gray-400"></div> */}
-                        <div className="flex justify-end">
+                        <div className="flex justify-end w-full 300:w-1/2">
                             {/* TODO: onclick delete */}
                             <Button variant="destructive" className="w-full">
                                 <X color="white" size={100} />
